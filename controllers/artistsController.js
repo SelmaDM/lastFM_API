7bb62f49a6707b85c2db0b0063fa2c5ca10367e8
SelmaDM <84981083+SelmaDM@users.noreply.github.com>
2023-01-19 11:49:16 +0100

    Add use case artist empty

--- controllers/artistsController.js
+++ controllers/artistsController.js
@@ -1,18 +1,28 @@
 const axios = require("axios");
 const dotenv = require('dotenv');
 const fs = require('fs')
-const json2csv = require('json-2-csv')
-dotenv.config({
-    path: './.env'
-});
+const {readFile} = require('fs');
+const path = './random.json';
+
+const json2csv = require('json-2-csv');
+dotenv.config({ path: './.env'});
 
 exports.getArtist = async (req, res) => {
 
     try {
+        //Function to convert json to csv
+        function jsonTOcsv(file) {
+            json2csv
+                .json2csvAsync(file)
+                .then(csv => {
+                    res.send("FILE SAVED")
 
-        const {
-            artistname
-        } = req.body;
+                    // write CSV to a file
+                    fs.writeFileSync('artistdata.csv', csv)
+                })
+        }
+
+        const {artistname} = req.body;
         const response = await axios.get(
             `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${artistname}&api_key=${process.env.LAST_FM}&format=json`
         );
@@ -21,28 +31,34 @@ exports.getArtist = async (req, res) => {
 
         //Filter the results to only include the artist informationZ
         const filtredArtistSearch = artistSearch.results.artistmatches.artist;
+        const numberOfArtists = JSON.parse(JSON.stringify(filtredArtistSearch)).length
+        if (numberOfArtists != 0) {
+            // Only return a subset of the data    
+            const subsetFields = filtredArtistSearch.map(item => {
+                return {
+                    name: item.name,
+                    mbid: item.mbid,
+                    url: item.url,
+                    image_small: item.image[0]['#text'],
+                    image: item.image[1]['#text']
+                }
+            });
 
-        // Only return a subset of the data    
-        const subsetFields = filtredArtistSearch.map(item => {
-            return {
-                name: item.name,
-                mbid: item.mbid,
-                url: item.url,
-                image_small: item.image[0]['#text'],
-                image: item.image[1]['#text']
-            }
-        });
-        console.log(subsetFields);
+            // convert JSON array to CSV string
+            jsonTOcsv(subsetFields)
 
-        // convert JSON array to CSV string
-        json2csv
-            .json2csvAsync(subsetFields)
-            .then(csv => {
-                res.send("FILE SAVED")
-
-                // write CSV to a file
-                fs.writeFileSync('artistdata.csv', csv)
+        } else { //Look for the artist in the random file and check their existence
+            readFile(path, (error, data) => {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+                data = JSON.parse(data);
+                const artist = data.find(artist => artist.name === artistname);
+                artist ? jsonTOcsv(artist) :
+                    res.send("Artist not found in random file")
             })
+        }
 
     } catch (error) {
         return res.status(500).json({
